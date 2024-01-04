ed5c0b11197179ca276fdb1c95e39b500262b4bc
Hansolo1103 <110121103@nitt.edu>
2024-01-05 00:21:01 +0530

    ecellSIPchanges

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,11 @@ import {db} from "./firebase.config"
 function App() {
 
   const [jobs,setJobs] = useState([])
+  const [customSearch, setCustomSearch] = useState(false);
 
   const fetchJobs = async() => {
 
+    setCustomSearch(false)
     const tempjobs = []
     const JobsRef = query(collection(db, "jobs"));
     const q = query(JobsRef, orderBy("postedOn" , "desc"), limit(3));
@@ -37,6 +39,8 @@ function App() {
   }
 
   const fetchJobsCustom = async(jobCriteria) => {
+
+    setCustomSearch(true)
 
     const tempjobs = []
     const JobsRef = query(collection(db, "jobs"));
@@ -70,6 +74,10 @@ function App() {
           <Navbar />
           <Header />
           <SearchBar fetchJobsCustom = {fetchJobsCustom}/>
+          {customSearch &&
+          <button onClick={fetchJobs} className="flex pl-[1250px] mb-2">
+              <p className="bg-red-500 px-10 py-2 rounded md text-white">Clear Filters</p>
+          </button>}
           {jobs.map((job) => (
             <JobCard key={job.id} {...job}/>
           ))}
